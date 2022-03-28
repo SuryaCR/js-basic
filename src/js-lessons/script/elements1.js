8121f8210c405f3f7402929a21a009e7fb0e31ea
Surya <suryamaverick1404@gmail.com>
2022-03-28 18:03:17 +0530

    commit

---
+++ src/js-lessons/script/elements1.js
@@ -0,0 +1,14 @@
+const firstName = document.getElementById('first-name');
+const aadharNo = document.getElementById('aadhar-no');
+const date = document.getElementById('date');
+const city = document.getElementById('city');
+const dateTest = document.getElementById('date-test');
+const time = document.getElementById('time');
+
+const positive = document.getElementById('result1');
+const negative = document.getElementById('result2');
+const labName = document.getElementById('lab-name');
+const reportNo = document.getElementById('report-no');
+const gender = document.getElementById('gender-male');
+const gender1 = document.getElementById('gender-female');
+const gender2 = document.getElementById('gender-others');
