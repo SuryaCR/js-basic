19f667f93d9325c6d56c35005c2ea461e90739ef
Surya <suryamaverick1404@gmail.com>
2022-03-28 10:51:53 +0530

    commit

--- src/js-lessons/script/currencies.js
+++ src/js-lessons/script/currencies.js
@@ -1,6 +1,4 @@
-import { math } from "./handlers.js";
-import { generateOptions } from "./money.js";
-const currencies = {
+export const currencies = {
   MSG: "Select a Country",
   USD: "United States Dollar",
   AUD: "Australian Dollar",
@@ -36,14 +34,3 @@ const currencies = {
   EUR: "Euro",
 };
 
-const from = document.querySelector("[name = from-country]");
-const to = document.querySelector("[name = to-country]");
-
-const country = generateOptions(currencies);
-from.innerHTML = country;
-to.innerHTML = country;
-
-document.getElementById("amount1").addEventListener("change", changeValue);
-function changeValue() {
-  math();
-}
--- src/js-lessons/script/element.js
+++ src/js-lessons/script/element.js
@@ -1,4 +1,14 @@
- const fromcurrency = document.getElementById('amount1').innerText;
- const tocurrency = document.getElementById('amount2').innerText;
-console.log(fromcurrency)
+import { math } from "./handlers.js";
+import { generateOptions } from "./money.js";
+import { currencies } from "./currencies.js";
+const from = document.querySelector("[name = from-country]");
+const to = document.querySelector("[name = to-country]");
 
+const country = generateOptions(currencies);
+from.innerHTML = country;
+to.innerHTML = country;
+
+document.getElementById("amount1").addEventListener("change", changeValue);
+function changeValue() {
+  math();
+}
