85f9296bc93271c8a2c1007ae969114371a8e36c
Surya <suryamaverick1404@gmail.com>
2022-03-28 09:25:36 +0530

    commit

--- src/js-lessons/script/handlers.js
+++ src/js-lessons/script/handlers.js
@@ -1,5 +1,5 @@
 export function math() {
     var value1 = document.getElementById("amount1");
-    var result = parseInt((Math.ceil(Math.random() * 100)) * value1.value);
+    var result = parseInt((Math.ceil(Math.random() * 10)) * value1.value);
     document.getElementById("amount2").innerHTML = result;
 }
